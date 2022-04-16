1ccc8e86a8e18c8fb6ad6381488b59972e0b401d
Irene Njeru <irenenjeru8@gmail.com>
2022-04-16 15:06:45 +0300

    task completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin(data){
+    let won= [];
+    for(let collection of data){
+        if(collection.result === "W"){
+            won.push(collection.year)
+        }
+    }
+    return won[0]
+}
+
+console.log(superbowlWin(record))
